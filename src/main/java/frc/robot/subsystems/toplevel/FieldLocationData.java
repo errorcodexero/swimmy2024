9301a74eb1b618186b290b2e8d823df2409fe5df
Jack (Butch) Griffin <butchg@comcast.net>
2023-11-07 15:58:24 -0800

    updated with vendor deps for 2024

--- src/main/java/frc/robot/subsystems/toplevel/FieldLocationData.java
+++ src/main/java/frc/robot/subsystems/toplevel/FieldLocationData.java
@@ -13,7 +13,6 @@ import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Pose3d;
 import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.geometry.Translation2d;
-import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.DriverStation.Alliance;
 import frc.robot.subsystems.toplevel.RobotOperation.GridTagPosition;
 import frc.robot.subsystems.toplevel.RobotOperation.Slot;
